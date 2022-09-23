bc8524e7efc62c74394ac557e279ab16ac509ecd
Vicente :^l <101284198+DryusFar@users.noreply.github.com>
2022-09-22 22:27:33 -0300

    si

--- src/app/services/bd.service.ts
+++ src/app/services/bd.service.ts
@@ -14,13 +14,7 @@ export class BDService {
 
    // variable para manipular la conexion a la base de datos
    public database: SQLiteObject;
-   //variable para la sentencia de creación de tabla
-   tablaTipousuario: string = "CREATE TABLE IF NOT EXISTS tipousuario(id_tipo INTEGER PRIMARY KEY, nombre_tipo VARCHAR(40) NOT NULL);";
-   //variable para la sentencia de registros por defecto en la tabla
-   registroTipousuario: string = "INSERT or IGNORE INTO tipousuario(id_tipo, nombre_tipo) VALUES (1,'Pasajero'),(2,'Chofer');";
-   //observable para manipular todos los registros de la tabla noticia
-   listaTipousuarios = new BehaviorSubject([]);
-   //observable para manipular si la BD esta lista  o no para su manipulación
+
    private isDBReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
  
    constructor(private sqlite: SQLite, private platform: Platform, private toastController: ToastController) { 
@@ -37,20 +31,14 @@ export class BDService {
      await toast.present();
    }
  
-   dbState() {
-     return this.isDBReady.asObservable();
-   }
- 
-   fetchTipousuarios(): Observable<Tipousuario[]> {
-     return this.listaTipousuarios.asObservable();
-   }
+   
  
    crearBD() {
      //verificamos que la plataforma este lista
      this.platform.ready().then(() => {
        //creamos la BD
        this.sqlite.create({
-         name: 'bdnoticias.db',
+         name: 'bdcheemy.db',
          location: 'default'
        }).then((db: SQLiteObject) => {
          //guardamos la conexion a la BD en la variable propia
@@ -66,13 +54,16 @@ export class BDService {
  
    async crearTablas() {
      try {
-       //ejecuto mis tablas
-       await this.database.executeSql(this.tablaTipousuario, []);
-       //cargar todos los registros de la tabla en el observable
-       await this.database.executeSql(this.registroTipousuario, []);
-       //actualizar el status de la BD
-       await this.database.executeSql(this.tablaUsuario, []);
-       this.isDBReady.next(true);
+        await this.database.executeSql(this.tablaTitulos, []);
+        //ejecuto mis tablas
+        await this.database.executeSql(this.tablaPuntajes, []);
+        
+        await this.database.executeSql(this.tablaTipousuario, []);
+        //cargar todos los registros de la tabla en el observable
+        await this.database.executeSql(this.registroTipousuario, []);
+        //actualizar el status de la BD
+        await this.database.executeSql(this.tablaUsuario, []);
+        this.isDBReady.next(true);
  
      } catch (e) {
        this.presentToast("Error Tablas: " + e);
@@ -114,11 +105,29 @@ export class BDService {
    fetchPuntaje(): Observable<Puntaje[]> {
      return this.listaPuntajes.asObservable();
    }
+
+   //-----------------------------------------------------------------------------------------------------------------------------------//
+
+   //variable para la sentencia de creación de tabla
+   tablaTipousuario: string = "CREATE TABLE IF NOT EXISTS tipousuario(id_tipo INTEGER PRIMARY KEY, nombre_tipo VARCHAR(40) NOT NULL);";
+   //variable para la sentencia de registros por defecto en la tabla
+   registroTipousuario: string = "INSERT or IGNORE INTO tipousuario(id_tipo, nombre_tipo) VALUES (1,'Pasajero'),(2,'Chofer');";
+   //observable para manipular todos los registros de la tabla noticia
+   listaTipousuarios = new BehaviorSubject([]);
+   //observable para manipular si la BD esta lista  o no para su manipulación
+   
+    dbState() {
+      return this.isDBReady.asObservable();
+    }
+
+    fetchTipousuarios(): Observable<Tipousuario[]> {
+      return this.listaTipousuarios.asObservable();
+    }
   
    //-----------------------------------------------------------------------------------------------------------------------------------//
 
    //variable para la sentencia de creación de tabla
-   tablaUsuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario VARCHAR(50) PRIMARY KEY, nombre VARCHAR(40) NOT NULL, apellido VARCHAR(40) NOT NULL, numero NUMBER NOT NULL, clave VARCHAR(25) NOT NULL, puntosusuario NUMBER, imagen File, FOREIGN KEY(id_tipo) REFERENCES tipouusuario(id_tipo));";
+   tablaUsuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario VARCHAR(50) PRIMARY KEY, nombre VARCHAR(40) NOT NULL, apellido VARCHAR(40) NOT NULL, numero NUMBER NOT NULL, clave VARCHAR(25) NOT NULL, puntosusuario NUMBER, imagen File, FOREIGN KEY(id_tipo) REFERENCES tipouusuario(id_tipo), FOREIGN KEY(id_puntaje) REFERENCES puntaje(id_puntaje));";
    //variable para la sentencia de registros por defecto en la tabla
    listaUsuarios = new BehaviorSubject([]);
    //observable para manipular si la BD esta lista  o no para su manipulación
--- src/app/services/puntaje.ts
+++ src/app/services/puntaje.ts
@@ -1,2 +1,4 @@
 export class Puntaje {
+    id_puntaje: number;
+    puntos: number;
 }
