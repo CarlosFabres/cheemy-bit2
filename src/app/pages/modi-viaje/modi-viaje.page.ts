5a897e578de0dd713e046880b4258fb14ba776a2
Vicente :^l <101284198+DryusFar@users.noreply.github.com>
2022-10-16 01:40:31 -0300

    asd
    asd

--- src/app/pages/modi-viaje/modi-viaje.page.ts
+++ src/app/pages/modi-viaje/modi-viaje.page.ts
@@ -61,7 +61,7 @@ export class ModiViajePage implements OnInit {
         this.servicioBD.presentToast("No puedes asignar un numero menor al de los asientos ya ocupados.");
       }else{
         this.servicioBD.modificarViajes(this.idviaje,this.horario,this.destino,this.tarifa,this.asientos,this.sector);
-        this.servicioBD.presentToast("Viaje Modificado.");
+        this.servicioBD.presentToast("Viaje Modificado con exito.");
       }
       
     }
